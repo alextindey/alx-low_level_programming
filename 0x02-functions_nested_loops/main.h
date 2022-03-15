d1c6cf509e21217ae46708c39be7ded880a09675
alextindey <eremchinedu43@gmail.com>
2022-03-15 21:23:27 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,25 +1,5 @@
 #include "main.h"
 
-/**
-*main - prints _putchar
-*
-*description: prints _putchar
-*
-*Returns: 0 (Success)
-*/
+int _putchar(void);
 
-
-int main(void)
-{
-
-	char text[9] = "_putchar";
-	int i = 0;
-
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(text[i]);
-	}
-	_putchar('\n');
-	
-	return (0);
-}
+#endif
